0e24fc211f91c66f9e291031b01275330db1723c
“PEILIN <2575240j@student.gla.ac.uk>
2023-03-03 10:47:08 +0000

    comment

--- groupproject/resell/models.py
+++ groupproject/resell/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Category(models.Model):
 	cateID = models.IntegerField(default = 20, unique = True)
-	cateName = modles.CharField(max_length = 128)
+	cateName = models.CharField(max_length = 128)
 
 	def __str__(self):
 		return self.cateID
@@ -12,15 +12,15 @@ class Category(models.Model):
 
 
 class product(modles.Modle):
-	prodID = modles.IntegerField(default = 20, unique = True)
-	prodName = modles.CharField(max_length = 100)
-	Brand = modles.CharField(max_length = 100)
-	Condition = modles.CharField(max_length = 100)
-	Price = modles.IntegerField()
-	Description = modles.CharField(max_length = 10000)
+	prodID = models.IntegerField(default = 20, unique = True)
+	prodName = models.CharField(max_length = 100)
+	Brand = models.CharField(max_length = 100)
+	Condition = models.CharField(max_length = 100)
+	Price = models.IntegerField()
+	Description = models.CharField(max_length = 10000)
 
 
 	def __str__(self):
-		retrun self.prodID
+		return self.prodID
 
 
