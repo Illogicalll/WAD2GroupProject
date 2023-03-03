1fe15a1804f833636156f9b2151c7a0eca4b1ec2
Robert Molloy <2651113m@student.gla.ac.uk>
2023-03-03 10:20:01 +0000

    fixed static files compatibility

--- groupproject/groupproject/settings.py
+++ groupproject/groupproject/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'groupproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
