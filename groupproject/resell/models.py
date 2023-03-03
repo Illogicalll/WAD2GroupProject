3edb79c87d8e97072339be71aa68b8586742c431
“PEILIN <2575240j@student.gla.ac.uk>
2023-03-03 10:35:12 +0000

    modle change

--- groupproject/resell/models.py
+++ groupproject/resell/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	cateID = models.IntegerField(default = 20, unique = True)
+	cateName = modles.CharField(max_length = 128)
+
+	def __str__(self):
+		return self.cateID
+
+
+
+class product(modles.Modle):
+	prodID = modles.IntegerField(default = 20, unique = True)
+	prodName = modles.CharField(max_length = 100)
+	Brand = modles.CharField(max_length = 100)
+	Condition = modles.CharField(max_length = 100)
+	Price = modles.IntegerField()
+	Description = modles.CharField(max_length = 10000)
+
+
+	def __str__(self):
+		retrun self.prodID
+
+
