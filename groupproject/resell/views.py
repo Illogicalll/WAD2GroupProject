ec282040f036a604bcdcea237476bbe86eac4e35
Caleb Mok <2689719m@student.gla.ac.uk>
2023-03-10 10:44:24 +0000

    updated views

--- groupproject/resell/views.py
+++ groupproject/resell/views.py
@@ -7,3 +7,25 @@ def index(request):
 
 def about(request):
     return render(request, 'resell/about.html')
+
+
+def sell(request):
+    return render(request, 'resell/sell.html')
+
+def buy(request):
+    return render(request, 'resell/buy.html')
+
+def login(request):
+    return render(request, 'resell/login.html')
+
+def products(request):
+    return render(request,'resell/products.html')
+
+
+""" 
+def product(request):
+    return render(request, 'resell/{product_id}.html')
+    
+def user(request):
+    return render(request, 'resell/{user_id}.html)
+"""
